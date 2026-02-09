0c50a2bc94169901e875507def273345f506743a
piotrekkonczyk <konczyk.piotrek@gmail.com>
2026-02-09 09:21:15 +0100

    bug form textarea

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,10 +1,18 @@
-import { Textarea } from "@chakra-ui/react"
+import { Box, Field, Textarea } from "@chakra-ui/react"
 
 function Form() {
   return (
-    <form action="">
-        <Textarea placeholder="Describe your bug (or a feature)..." />
-    </form>
+    <Box p={"8"} border={"1px solid #ddd"} borderRadius={"sm"} minW={"400px"}>
+        <form action="">
+            <Field.Root>
+                <Field.Label>
+                    Bug or a feature
+                    <Field.RequiredIndicator />
+                </Field.Label>
+                <Textarea placeholder="Describe your bug (or a feature)..." />
+            </Field.Root>
+        </form>
+    </Box>
   )
 }
 
