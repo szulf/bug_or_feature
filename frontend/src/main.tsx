575b0eb4ee2210634aa88b13ea8bb380555dac67
piotrekkonczyk <konczyk.piotrek@gmail.com>
2026-02-11 11:46:16 +0100

    post creation data zod schema and on submit function

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Provider from './components/Provider.tsx'
+import { Toaster } from './components/ui/toaster.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
+      <Toaster />
     <App />
     </Provider>
   </StrictMode>,
