f3dce37923fb66787f079f5605e83fa67ee38af9
piotrekkonczyk <konczyk.piotrek@gmail.com>
2026-02-16 09:25:52 +0100

    bug of the week tab and create form adjusting

--- frontend/src/components/PageTabs.tsx
+++ frontend/src/components/PageTabs.tsx
@@ -1,10 +1,10 @@
 import { Tabs } from '@chakra-ui/react'
-import { LuLibraryBig, LuFilePlus } from "react-icons/lu"
+import { LuLibraryBig, LuFilePlus, LuTrophy } from "react-icons/lu"
 import Form from './Form'
 
 function PageTabs() {
   return (
-    <Tabs.Root defaultValue="all_posts" minW={"2xl"}>
+    <Tabs.Root defaultValue="all_posts" w={"75%"}>
       <Tabs.List>
         <Tabs.Trigger value="all_posts">
           <LuLibraryBig />
@@ -14,13 +14,21 @@ function PageTabs() {
           <LuFilePlus />
           Create new post
         </Tabs.Trigger>
+        <Tabs.Trigger value="bug_of_the_week">
+          <LuTrophy />
+          Bug of the week
+        </Tabs.Trigger>
       </Tabs.List>
       <Tabs.Content value="all_posts">
-        {/* Todo: create all posts component*/}
+        {/* TODO: create all posts component*/}
         All posts will go here
       </Tabs.Content>
       <Tabs.Content value="create_new_post">
         <Form />
+      </Tabs.Content>
+      <Tabs.Content value="bug_of_the_week">
+        {/* TODO: create all posts component*/}
+        Bug of the week will go here
       </Tabs.Content>
     </Tabs.Root>
   )
